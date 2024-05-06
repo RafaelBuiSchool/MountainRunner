e53b2bbaa227c2eb4c9e98f7157823541ab5123a
BT_4N2_16 <rafaelb54@Nycstudents.net>
2024-05-06 13:58:42 -0400

    Scrapped the idea of 4 levels and made it so there is an easy, medium and hard level that the user can choose. Working on the level choosing screen where they can select one of those levels, and added a new sprite background.

--- src/HomeScreenPainter.java
+++ src/HomeScreenPainter.java
@@ -10,19 +10,14 @@ import java.io.IOException;
 
 
 public class HomeScreenPainter extends JPanel implements MouseListener{
-
-    private Rectangle continueGame;
     private Rectangle newGame;
     private Rectangle exit;
     private NewGame game;
-    private ContinuedGame currentGame;
     public HomeScreenPainter(){
-        continueGame = new Rectangle(800, 400, 320, 100);
-        newGame = new Rectangle(800, 560, 320, 100);
-        exit = new Rectangle(800, 720, 320, 100);
+        newGame = new Rectangle(800, 410, 320, 100);
+        exit = new Rectangle(800, 570, 320, 100);
         this.addMouseListener(this);
         this.game = game;
-        this.currentGame = currentGame;
     }
     protected void paintComponent(Graphics g) {
         BufferedImage homeScreen = null;
@@ -35,22 +30,16 @@ public class HomeScreenPainter extends JPanel implements MouseListener{
         g.setFont(new Font("Broadway", Font.BOLD, 50));
         g.drawString("MOUNTAIN RUNNER", 705, 305);
         g.setFont(new Font("Broadway", Font.BOLD, 50));
-        g.drawString("CONTINUE", 820, 465);
-        g.drawRect((int)continueGame.getX(), (int)continueGame.getY(), (int)continueGame.getWidth(), (int)continueGame.getHeight());
-        g.setFont(new Font("Broadway", Font.BOLD, 50));
-        g.drawString("NEW GAME", 815, 625);
+        g.drawString("NEW GAME", 815, 475);
         g.drawRect((int)newGame.getX(), (int)newGame.getY(), (int)newGame.getWidth(), (int)newGame.getHeight());
         g.setFont(new Font("Broadway", Font.BOLD, 50));
-        g.drawString("EXIT", 890, 785);
+        g.drawString("EXIT", 890, 635);
         g.drawRect((int)exit.getX(), (int)exit.getY(), (int)exit.getWidth(), (int)exit.getHeight());
     }
     public void mousePressed(MouseEvent e){
         Point clicked = e.getPoint();
         if(e.getButton()==1){
-            if(continueGame.contains(clicked)){
-                currentGame = new ContinuedGame("Levels (CONTINUED GAME)");
-            }
-            else if(newGame.contains(clicked)){
+            if(newGame.contains(clicked)){
                 game = new NewGame("Levels (NEW GAME)");
             }
             else if(exit.contains(clicked)){
